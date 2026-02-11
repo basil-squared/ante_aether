7293dac4c48c7cbfe89ef947e0aba4ae47e8270f
Juniper <basil.squared1@gmail.com>
2026-02-11 06:35:49 -0600

    fix: try to fix repeater (wip)

--- modules/content/jokers/resonance/rare/repeater.lua
+++ modules/content/jokers/resonance/rare/repeater.lua
@@ -10,7 +10,7 @@ SMODS.Joker {
     end,
     calculate = function(self, card, context)
         if context.repetition and context.cardarea == G.play then
-            if next(context.poker_hands['High Card']) then
+            if context.scoring_name == 'High Card' then
                 return {
                     repetitions = card.ability.extra.repetitions,
                     message = localize('k_again_ex')
@@ -18,13 +18,23 @@ SMODS.Joker {
             end
         end
         if context.after and not context.blueprint and not context.retrigger_joker then
-            if next(context.poker_hands['High Card']) then
+            if context.scoring_name == 'High Card' and G.GAME.current_round.hands_left > 0 and not G.GAME.blind.defeated then
                 local held_cards = {}
                 for _, v in ipairs(G.hand.cards) do
                     held_cards[#held_cards + 1] = v
                 end
                 if #held_cards > 0 then
-                    SMODS.destroy_cards(held_cards)
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.2,
+                        func = function()
+                            for _, v in ipairs(held_cards) do
+                                G.hand:add_to_highlighted(v)
+                            end
+                            G.FUNCS.discard_cards_from_highlighted()
+                            return true
+                        end
+                    }))
                 end
             end
         end
